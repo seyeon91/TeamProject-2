1dafe18512e0fe6d7f85f5f017b34ae573f868f9
2023320004 호준수 <hojunssss18293@gmail.com>
2026-06-09 20:29:53 +0900

    AlcoholicDrinks 클래스 생성

---
+++ AlcoholicDrinks.java
@@ -0,0 +1,32 @@
+
+/**
+ * 계산시 주류세를 적용하는 클래스
+ *
+ * @author (2025320070_복창희, 2025320038_윤세연, 2023320022_편규빈, 2023320004_호준수)
+ * @version (2026.06.09)
+ */
+public class AlcoholicDrinks extends Products implements TAX {
+    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
+    private int x;
+
+    /**
+     * AlcoholicDrinks 클래스의 객체 생성자
+     */
+    public AlcoholicDrinks()
+    {
+        // 인스턴스 변수 초기화
+        x = 0;
+    }
+
+    /**
+     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public int sampleMethod(int y)
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+}
