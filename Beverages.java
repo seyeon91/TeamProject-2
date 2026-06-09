33cd63288f59491c4f7d2a6ae47785e95e07ae38
Magcargo9999 <131877946+Magcargo9999@users.noreply.github.com>
2026-06-09 20:16:00 +0900

    Beverages 클래스 생성

---
+++ Beverages.java
@@ -0,0 +1,12 @@
+
+/**
+ * 계산시 부가세를 적용하는 클래스
+ *
+ * @author (2025320070_복창희, 2025320038_윤세연, 2023320022_편규빈, 2023320004_호준수)
+ * @version (2026.06.09)
+ */
+
+public class Beverages extends Products implements TAX {
+    
+  
+}
