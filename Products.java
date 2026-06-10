83cbde458e616d61dd5f77d729e9b8e88523024c
seyeon91 <seyeony91@gmail.com>
2026-06-10 14:16:10 +0900

    Products 추상화, 생성자 작성 완료

--- Products.java
+++ Products.java
@@ -5,7 +5,21 @@
  * @author (2025320070_복창희, 2025320038_윤세연, 2023320022_편규빈, 2023320004_호준수)
  * @version (2026.06.09)
  */
-public class Products
+
+// 모든 상품의 부모 클래스(추상 클래스)
+// 직접 객체를 만들 수 없고, 반드시 자식 클래스를 통해 사용
+public abstract class Products
 {
+    String barcode;
+    String name;
+    int price;
     
+    // 생성자: 바코드, 상품명, 단가를 받아서 저장
+    public Products(String b, String n, int p){
+        barcode = b;
+        name = n;
+        price = p;
+    }
+    
+    public abstract int calcPrice();
 }
