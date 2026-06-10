15f41f6ce8a520c6e4a19fc49dda4ad9fdbd847a
Magcargo9999 <131877946+Magcargo9999@users.noreply.github.com>
2026-06-10 14:27:21 +0900

    Beverages 코드 작성완료

--- Beverages.java
+++ Beverages.java
@@ -5,9 +5,15 @@
  * @author (2025320070_복창희, 2025320038_윤세연, 2023320022_편규빈, 2023320004_호준수)
  * @version (2026.06.09)
  */
-
+// 음료수 클래스 : Products 상속, TAX 인터페이스 구현
 public class Beverages extends Products implements TAX{
-    
-    
-  
+    //생성자
+    public Beverages(String b, String n, int p){
+        super(b, n, p);
+    }
+    //부가세 10% 적용 : 단가 * 1.1
+    public int calcPrice(){
+        return (int)(price * 1.1);
+    }
+
 }
