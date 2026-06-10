0dadb914408521af9d2955d9c2cb627a9bb7dcee
2023320004 호준수 <hojunssss18293@gmail.com>
2026-06-10 14:36:00 +0900

    AlcoholicDrinks 클래스 생성자 호출 코드 작성 및  주류세 계산 코드 작성

--- AlcoholicDrinks.java
+++ AlcoholicDrinks.java
@@ -5,28 +5,14 @@
  * @author (2025320070_복창희, 2025320038_윤세연, 2023320022_편규빈, 2023320004_호준수)
  * @version (2026.06.09)
  */
+// 주류 클래스 : Products 상속, TAX 인터페이스 구현
 public class AlcoholicDrinks extends Products implements TAX {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
-    private int x;
-
-    /**
-     * AlcoholicDrinks 클래스의 객체 생성자
-     */
-    public AlcoholicDrinks()
-    {
-        // 인스턴스 변수 초기화
-        x = 0;
+    // 생성자 : 부모 생성자(products) 호출
+    public AlcoholicDrinks(String b, String n, int p){
+        super(b, n, p);
     }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 더하기 y의 결과값을 반환
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하세요.
-        return x + y;
+    // 부가세 10% + 주세 30% = 40% 적용 : 단가 * 1.4
+    public int calcPrice(){
+        return (int)(price * 1.4);
     }
 }
