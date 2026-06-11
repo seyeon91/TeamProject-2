3f8ac3302532e3fe640b4890e2710114d1eaa41c
seyeon91 <seyeony91@gmail.com>
2026-06-12 05:18:46 +0900

    Products, MyApp 주석 작성

--- MyApp.java
+++ MyApp.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 /**
- * MyApp은 코드 작동을 위한 메인 클래스
+ * MyApp는 메인 클래스로 POST 생성 후 거래 1회 처리
  *
  * @author (2025320070_복창희, 2025320038_윤세연, 2023320022_편규빈, 2023320004_호준수)
- * @version (2026.06.09)
+ * @version (2026.06.12)
  */
-// POST 객체를 생성하고 바코드 입력 루프 실행
 public class MyApp
 {
+    /**
+     * 바코드를 반복 입력 받아 거래를 처리하고 결제한다.
+     */
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         POST post = new POST();
         
         System.out.println("바코드 입력 (끝: 0)");
-        
         while(true){
             System.out.print("바코드: ");
             String b = scan.next();
@@ -26,6 +27,6 @@ public class MyApp
                 System.out.println("없는 바코드.");
             }
         }
-        post.pay(scan);
+        post.pay(scan); // 결제 + DB저장
     }
 }
--- Products.java
+++ Products.java
@@ -3,7 +3,7 @@
  * Products는 상품 목록 저장하는 클래스
  *
  * @author (2025320070_복창희, 2025320038_윤세연, 2023320022_편규빈, 2023320004_호준수)
- * @version (2026.06.09)
+ * @version (2026.06.12)
  */
 
 // 모든 상품의 부모 클래스(추상 클래스)
@@ -14,5 +14,8 @@ public abstract class Products
     String name;
     int price;
 
+    /**
+     * 상품의 세금을 계산하는 추상 메소드
+     */
     public abstract int calcTax();
 }
